252a03a3e3bbef063efdf9dc4bdd6e22619eccf1
TruongQuocAn <69239963+ITDSIU19001@users.noreply.github.com>
2021-12-03 22:14:59 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ import { initAPIs } from './routes/api.js'
 
 const app = express()
 
-// Cho phép lý dữ liệu từ form method POST
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-// Khởi tạo các APIs cho ứng dụng
+
 initAPIs(app)
 
-// chọn một port mà bạn muốn và sử dụng để chạy ứng dụng tại local
+
 const port = 8017
 app.listen(port, () => {
   console.log(`I'm running at localhost:${port}/`)
